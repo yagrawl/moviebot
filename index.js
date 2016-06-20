4033012700f01f25144008db0887131cc08ab835
Yash Agrawal <yagrawl@Yashs-MacBook-Pro.local>
2016-06-20 21:25:02 +0530

    Implemented Basic Switch

--- index.js
+++ index.js
@@ -26,9 +26,6 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
         switch(event.message.text) {
         	
         	case 0:
