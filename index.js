66fce15c7bc4f3d7f5243a4531b37145e794959a
Yash Agrawal <yagrawl@Yashs-MacBook-Pro.local>
2016-06-21 17:32:45 +0530

    Added movie request

--- index.js
+++ index.js
@@ -49,10 +49,10 @@ app.post('/webhook', function (req, res) {
         			sendMessage(event.sender.id, {text: "Hello!"});
         			break;
 
-                case "movie":
-                    requestMovie();
-                    sendMessage(event.sender.id, {text: title});
-                    break;
+                // case "movie":
+                //     requestMovie();
+                //     sendMessage(event.sender.id, {text: title});
+                //     break;
 
         		case "Thanks":
         		case "thank you":
@@ -124,23 +124,23 @@ function sendMessage(recipientId, message) {
     });
 };
 
-function requestMovie(){
+// function requestMovie(){
 
-    module.exports = function (callback){
+//     module.exports = function (callback){
 
-    }
-    request({
-        method: 'GET', json:true,
-        url: baseUrl + mId + APIkey,
-        headers: {
-            'Accept': 'application/json'
-        }}, function (error, response, body) {
-  //console.log('Response:', body);
-  //console.log('Original Title:', body.original_title);
-  //console.log('Popularity: ', body.popularity);
-        var title = body.original_title ;
-    });
-};
+//     }
+//     request({
+//         method: 'GET', json:true,
+//         url: baseUrl + mId + APIkey,
+//         headers: {
+//             'Accept': 'application/json'
+//         }}, function (error, response, body) {
+//   //console.log('Response:', body);
+//   //console.log('Original Title:', body.original_title);
+//   //console.log('Popularity: ', body.popularity);
+//         var title = body.original_title ;
+//     });
+// };
 //Math.floor((Math.random() * 30) - 1)
 
 function sendImage(recipientId, message) {
