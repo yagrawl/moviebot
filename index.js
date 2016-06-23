1f42f64cf87b39a078f97f21e3b719b90eb2ec49
Yash Agrawal <yagrawl@Yashs-MacBook-Pro.local>
2016-06-23 18:40:01 +0530

    Status code

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ function getMovie(recipientId) {
             //console.log('Headers:', JSON.stringify(response.headers));
            // console.log('Response:', body);
             //console.log('Check:', body);
-            sendMessage(recipientId, response.statusCode);
+            sendMessage(recipientId, {text: response.statusCode});
         });
         //var title = JSON.stringify(response.headers);
         
