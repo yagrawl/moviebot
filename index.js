fc2af5150c8ba9a4fce8741b693a8f492fe40660
Yash Agrawal <yagrawl@Yashs-MacBook-Pro.local>
2016-06-23 18:33:35 +0530

    Headers

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ function getMovie(recipientId) {
             //console.log('Headers:', JSON.stringify(response.headers));
            // console.log('Response:', body);
             //console.log('Check:', body);
-            sendMessage(recipientId, {text: response.body.original_title} );
+            sendMessage(recipientId, {text: SON.stringify(response.headers)} );
         });
         //var title = JSON.stringify(response.headers);
         
