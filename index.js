47ca455e34c8323fc90aa00a41887b86f644eac3
Agrawal <code@getbraintree.com>
2018-08-03 10:41:58 -0700

    post to webhook

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/webhook', function (req, res) {
   }
 });
 
-app.post('/', (req, res) => {
+app.post('/webhook', (req, res) => {
   let events = req.body.entry[0].messaging;
   for (i = 0; i < events.length; i++) {
     let event = events[i];
